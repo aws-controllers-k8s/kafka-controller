43ac08cb2f882fb99f4941a0fb74033f7f3efcc4
ci-robot <ci-robot@k8s.io>
2026-01-07 18:43:30 +0000

    Update to ACK runtime `v0.57.0`, code-generator `v0.57.0`

--- pkg/resource/cluster/delta.go
+++ pkg/resource/cluster/delta.go
@@ -17,16 +17,15 @@ package cluster
 
 import (
 	"bytes"
-	"reflect"
 
 	ackcompare "github.com/aws-controllers-k8s/runtime/pkg/compare"
 	acktags "github.com/aws-controllers-k8s/runtime/pkg/tags"
+	"k8s.io/apimachinery/pkg/api/equality"
 )
 
 // Hack to avoid import errors during build...
 var (
 	_ = &bytes.Buffer{}
-	_ = &reflect.Method{}
 	_ = &acktags.Tags{}
 )
 
@@ -44,7 +43,7 @@ func newResourceDelta(
 	}
 	customPreCompare(delta, a, b)
 
-	if !reflect.DeepEqual(a.ko.Spec.AssociatedSCRAMSecretRefs, b.ko.Spec.AssociatedSCRAMSecretRefs) {
+	if !equality.Semantic.Equalities.DeepEqual(a.ko.Spec.AssociatedSCRAMSecretRefs, b.ko.Spec.AssociatedSCRAMSecretRefs) {
 		delta.Add("Spec.AssociatedSCRAMSecretRefs", a.ko.Spec.AssociatedSCRAMSecretRefs, b.ko.Spec.AssociatedSCRAMSecretRefs)
 	}
 	if len(a.ko.Spec.AssociatedSCRAMSecrets) != len(b.ko.Spec.AssociatedSCRAMSecrets) {
